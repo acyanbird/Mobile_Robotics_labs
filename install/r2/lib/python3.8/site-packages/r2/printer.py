e6f70e4d461830aa13eba1543fcf59acd8916856
acyanbird <acyanbird@gmail.com>
2024-02-03 16:50:45 +0000

    task2

--- install/r2/lib/python3.8/site-packages/r2/printer.py
+++ install/r2/lib/python3.8/site-packages/r2/printer.py
@@ -3,7 +3,7 @@ import rclpy
 from rclpy.node import Node
 
 rclpy.init(args=None)
-node = Node('printer_node')
+node = Node('printer_node')  # Note that there should have no space in node name
 node.get_logger().info('HELLO WORLD - ROS2')
 rclpy.spin(node)
 node.destroy_node()
