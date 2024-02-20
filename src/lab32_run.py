7f3ff47995f64bc994fee934847321805cd93687
acyanbird <acyanbird@gmail.com>
2024-02-20 18:57:31 +0000

    update

--- src/lab32_run.py
+++ src/lab32_run.py
@@ -6,4 +6,4 @@ for _ in range(10):
     # run with low noise
     subprocess.run(["ros2", "run", "lab32", "move", "--ros-args", "-p", "l_noise:=0.002", "-p", "a_noise:=0.001", "-p", "file_name:=p2.txt"])
     # run with high noise
-    subprocess.run(["ros2", "run", "lab32", "move", "--ros-args", "-p", "l_noise:=0.02", "-p", "a_noise:=0.005", "-p", "file_name:=p3.txt"])
+    subprocess.run(["ros2", "run", "lab32", "move", "--ros-args", "-p", "l_noise:=0.02", "-p", "a_noise:=0.01", "-p", "file_name:=p3.txt"])
